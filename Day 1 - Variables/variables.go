5b9e24e05b48f560ae4468bc958bb6e8abf6638e
Mrinmayee Rane <mrinmayeerane2810@gmail.com>
2024-03-24 01:41:53 -0400

    Day-1

---
+++ Day 1 - Variables/variables.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// basic types
+	// int, float32, bool, string
+	//1. var name type = value
+	var x string = "code"
+	var y = 2
+	var z bool
+	//2. name := value
+	a := "withmrin"
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+	fmt.Println(a)
+}
