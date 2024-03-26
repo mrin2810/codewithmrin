095600a36c9076ffea43ceb7a935afab73104c5e
Mrinmayee Rane <mrinmayeerane2810@gmail.com>
2024-03-26 17:20:19 -0400

    day 3

---
+++ Day 3 - Output/output.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+func main() {
+	// Variables defined
+	var i = "Hello"
+	// fmt.Print()
+	fmt.Print(i)
+	// fmt.Println()
+	fmt.Println(i)
+	// fmt.Printf()
+	fmt.Printf("i variable is of type: %T and has value %v.", i, i)
+
+}
