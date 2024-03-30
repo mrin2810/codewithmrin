70fb3d8d448bfc1cdf32a5a72bf10152e02ff141
Mrinmayee Rane <mrinmayeerane2810@gmail.com>
2024-03-29 21:32:11 -0400

    day - 4

---
+++ Day 4 - Arrays/arrays.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name = [size]type{vals}
+	var arr = [3]int{1, 2, 3}
+	var arr1 = [3]int{}
+	var arr2 = []int{2, 3, 4}
+
+	fmt.Println(arr[0])
+	fmt.Println(arr1[1])
+	fmt.Println(arr2[3])
+		
+
+}
